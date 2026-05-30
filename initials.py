aae7f44feed390805645358338284c834f66163f
Paula Yenube Bandjie <2425402408@live.gctu.edu.gh>
2026-05-30 21:52:43 +0000

    Assignment 1 submission
    The main function now prints the initials 'PYB' in a stylized large format, enhancing visual representation.

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Print out my initials
+# Assignment Number: 1
+#
+# Name: <PAULA YENUBE BANDJIE>
+# STUDENT ID: <2425402408>
+# Email: <2425402408@LIVE.GCTU.EDU.GH>
+# Grader: Augustus
+#
+# On my honor, <PAULA YENUBE BANDJIE>, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
-def main():
-  pass #code goes here
 
+def main(): 
+ # This function prints the initials PYB in a stylized large format.
+    print()
+    print("...PYB")
+    print()
+    print("...PPPPPPPPPP.........Y Y......Y Y.........BBBBBBBBBB....")
+    print("...PPPPPPPPPPP.........Y Y....Y Y..........BBBBBBBBBBB...")
+    print("...PP.......PP..........Y Y..Y Y...........BB.......BB...")
+    print("...PP.......PP...........Y YY Y............BB.......BB...")
+    print("...PPPPPPPPPPP............Y Y..............BBBBBBBBBBB...")
+    print("...PPPPPPPPPP.............Y Y..............BBBBBBBBBB....")
+    print("...PP.....................Y Y..............BB.......BB...")
+    print("...PP.....................Y Y..............BB.......BB...")
+    print("...PP........**...........Y Y........**....BBBBBBBBBBB..**")
+    print("...PP........**...........Y Y........**....BBBBBBBBBB...**")
+    print()
 
 
 main()
